2a45a5d0e60575c6884cc87cf3ed67b31a26aa68
Takumi Yamaguchi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2025-02-08 02:53:57 +0900

    A問題回答完了

--- A_Jiro.py
+++ A_Jiro.py
@@ -4,6 +4,23 @@ AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_a
 '''
 #入力を受け取る
-
+a, b, c = input().split()
 
 #出力
+if a == '<':
+    if b == '<':
+        if c == '<':
+            print('B')
+        else:
+            print('C')
+    else:
+        print('A')
+else:
+    if b == '<':
+        print('A')
+    else:
+        if c == '<':
+            print('C')
+        else:
+            print('B')
+        
