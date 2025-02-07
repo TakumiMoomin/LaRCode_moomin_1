a1a99a612f59ced37279120392d21b2c7bff8a86
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2025-02-07 23:51:58 +0900

    first commit

---
+++ A_Jiro.py
@@ -0,0 +1,9 @@
+'''
+2024-09-14(土) 21:00 ~ 2024-09-14(土) 22:40 (100分)
+AtCoder Beginner Contest 371
+https://atcoder.jp/contests/abc371/tasks/abc371_a
+'''
+#入力を受け取る
+
+
+#出力
---
+++ B_Cut0.py
@@ -0,0 +1,11 @@
+'''
+2024-08-17(土) 21:00 ~ 2024-08-17(土) 22:40 (100分)
+AtCoder Beginner Contest 367
+https://atcoder.jp/contests/abc367/tasks/abc367_b
+'''
+#入力を受け取る
+
+
+#出力
+
+
