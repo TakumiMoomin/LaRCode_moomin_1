d55a782f144a74d408de87d00a19d4f71c20276f
Takumi Yamaguchi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2025-02-20 09:57:33 +0900

    B問題回答済み

--- B_Taro.py
+++ B_Taro.py
@@ -4,8 +4,17 @@ AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_b
 '''
 #入力の受け取り
-
+n, m = map(int, input().split())
+str_list = []
+for i in range(m):
+    str_list.append(list(input().split()))
 
 #出力
-
+first_list = [list() for _ in range(n)]
+for i in range(m):
+    if first_list[int(str_list[i][0])-1] == [] and str_list[i][1] == 'M':
+        first_list[int(str_list[i][0]-1)].append('M')
+        print('Yes')
+    else:
+        print('No')
 
