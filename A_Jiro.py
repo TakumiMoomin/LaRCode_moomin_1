334fc7531049318dcb73c82f73c753e2d18a15fb
kkito <k.kito0726@gmail.com>
2025-03-12 20:15:49 +0900

    fmt

--- A_Jiro.py
+++ A_Jiro.py
@@ -1,9 +1,10 @@
-'''
+"""
 2024-09-14(土) 21:00 ~ 2024-09-14(土) 22:40 (100分)
 AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_a
-'''
-#入力を受け取る
+"""
+
+# 入力を受け取る
 
 
-#出力
+# 出力
--- B_Taro.py
+++ B_Taro.py
@@ -1,11 +1,10 @@
-'''
+"""
 2024-09-14(土) 21:00 ~ 2024-09-14(土) 22:40 (100分)
 AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_b
-'''
-#入力の受け取り
+"""
+
+# 入力の受け取り
 
 
-#出力
-
-
+# 出力
