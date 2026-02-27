7df62362a969a01796626422ecddb5950a73a54f
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-02-27 12:04:15 +0900

    A問題回答完了

--- A_Jiro.py
+++ A_Jiro.py
@@ -4,6 +4,12 @@ AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_a
 '''
 #入力を受け取る
-
+a, b, c, = input().split()
 
 #出力
+if a != b:
+    print('A')
+elif a == c:
+    print('B')
+else:
+    print('C')
