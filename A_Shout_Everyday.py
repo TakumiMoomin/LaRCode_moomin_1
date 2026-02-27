74a77af49088922aadc07e18c113eb2ae26114b1
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-02-27 23:28:45 +0900

    A問題回答完了

--- A_Shout_Everyday.py
+++ A_Shout_Everyday.py
@@ -4,8 +4,16 @@ AtCoder Beginner Contest 367
 https://atcoder.jp/contests/abc367/tasks/abc367_a
 '''
 #入力を受け取る
-
+a, b, c = map(int, input().split())
 
 #出力
-
-
+if b < c:
+    if b < a < c:
+        print('No')
+    else:
+        print('Yes')
+else:
+    if c < a < b:
+        print('Yes')
+    else:
+        print('No')
