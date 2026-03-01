f8f02fb655a03b6445f5cbe7616c4794cdef06a6
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-03-02 01:01:19 +0900

    B問題回答完了

--- B_Taro.py
+++ B_Taro.py
@@ -4,8 +4,16 @@ AtCoder Beginner Contest 371
 https://atcoder.jp/contests/abc371/tasks/abc371_b
 '''
 #入力の受け取り
-
+n, m = map(int, input().split())
 
 #出力
-
-
+first_son = [False]*(n)
+for _ in range(m):
+    a, b = input().split()
+    a = int(a)
+    
+    if b == "M" and not first_son[a-1]:
+        print("Yes")
+        first_son[a-1] = True
+    else:
+        print("No")
